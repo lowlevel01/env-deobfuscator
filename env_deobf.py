c21fa767260c1fd8612fce05b164753b4fc65386
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-02-24 16:50:55 +0000

    Create env_deobf.py

---
+++ env_deobf.py
@@ -0,0 +1,50 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("USAGE: python env_deobf.py [FILENAME]")
+    sys.exit()
+    
+def parse_line(line):
+    variable_list = []
+    percent = False
+    temp = ""
+    for char in line:
+        if char != "%" and percent == True:
+            temp += char
+        if char == "%" and percent == False:
+            percent = True
+            continue
+        if char == "%" and percent == True:
+            percent = False
+            variable_list.append(temp)
+            temp = ""
+    return variable_list
+
+variables = dict()
+file = open(sys.argv[1],"r").readlines()
+
+for line in file:
+    line = line.strip()
+    if "%" not in line and "Set" not in line:
+        continue
+
+    if "Set" in line:
+        name = line.split(" ")[1].split("=")[-2]
+        value = line.split(" ")[1].split("=")[-1]
+        variables[name] = value
+        continue
+    if "%" in line:
+        for variable in parse_line(line):
+            if variable in variables:
+                line = line.replace("%"+variable+"%", variables[variable])
+    
+        
+        if "Set" in line:
+            name = line.split(" ")[1].split("=")[0]
+            value = line.split(" ")[1].split("=")[1]
+            variables[name] = value
+            
+    print(line)
+
+    
+        
